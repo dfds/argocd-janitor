fece52fbc76519daf2f81a34502ee165b917a380
JacobHeidelbachDFDS <jfhei@dfds.com>
2019-03-11 20:32:56 +0100

    Housekeeping

--- db/migrations/20190224220857_create_capability_table.sql
+++
@@ -1,7 +0,0 @@
--- 2019-02-24 22:08:57 : create_capability_table
-CREATE TABLE public."Capability" (
-    "Id" uuid NOT NULL,
-    "Name" varchar(255) NOT NULL,
-    "AzureADObjectId" varchar(255) NULL,
-    CONSTRAINT capability_pk PRIMARY KEY ("Id")
-);
