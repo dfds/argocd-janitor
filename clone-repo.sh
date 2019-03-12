b2d5ff90789016f4c4fb76b2a2830fbececc4cb1
JacobHeidelbachDFDS <jfhei@dfds.com>
2019-03-12 12:38:18 +0100

    Added git repo as variable

--- clone-repo.sh
+++ clone-repo.sh
@@ -3,6 +3,7 @@
 set -eu
 
 GITREPO=$1
+TAG=$2
 
 
 # Exit if variable is empty
@@ -12,10 +13,23 @@ then
     exit 1
 fi
 
+# Exit if variable is empty
+if [ -z "$TAG" ]
+then
+    echo "\$TAG is empty"
+    exit 1
+fi
+
 mkdir gitops
 cd gitops
 
-git clone $GITREPO
+git clone $GITREPO .
+
+cd overlays/production
+
+kustomize edit set image 579478677147.dkr.ecr.eu-central-1.amazonaws.com/ded/argojanitor:$TAG
+git add . -m "Set image tag to $TAG"
+git push
 
 #kustomize edit ..
 
